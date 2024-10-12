379369fa03fd5ff86b718d4354be3871aa996a70
Yevhenii <jenmikitenko@gmail.com>
2024-10-12 20:36:37 +0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+/* import { useState } from "react";
+ */import "./App.css";
 /* import SC from 'soundcloud' */
 
 //var SC = require('soundcloud');
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
   const getPlaylists = async () => {
     fetch(
       "https://secure.soundcloud.com/authorize \
