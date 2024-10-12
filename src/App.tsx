fc20f3e30fca02e1a7693fe101c448e7d1a10a2a
Yevhenii <jenmikitenko@gmail.com>
2024-10-12 20:45:06 +0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* import { useState } from "react";
- */import "./App.css";
+ */ import "./App.css";
 /* import SC from 'soundcloud' */
 
 //var SC = require('soundcloud');
@@ -8,20 +8,14 @@ function App() {
   //const [count, setCount] = useState(0);
   const getPlaylists = async () => {
     fetch(
-      "https://secure.soundcloud.com/authorize \
-      ?client_id=5xP6vJj22M5hgD6GIEsszTFtbPJsU4zY \
-      &redirect_uri=http://localhost:5173 \
-      &response_type=code \
-      &code_challenge=CODE_CHALLENGE \
-      &code_challenge_method=S256 \
-      &state=STATE",{
-        method:"GET",
-        headers:{
-          "Access-Control-Allow-Origin": '*'
-        }
+      "https://secure.soundcloud.com/authorize?client_id=5xP6vJj22M5hgD6GIEsszTFtbPJsU4zY&redirect_uri=http://localhost:5173",
+      {
+        method: "GET",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
       }
-    ).then(res=>console.log(res))
-    
+    ).then((res) => console.log(res));
   };
   return (
     <>
