fd4a1194d62b1deffb0bbd56cabad5cbb0be799e
Yevhenii <jenmikitenko@gmail.com>
2024-10-28 16:09:31 +0200

    add some features on mediasession

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import dynamicImport from 'vite-plugin-dynamic-import';
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    dynamicImport(),
     VitePWA({
       manifest: {
         name: "ScFM",
@@ -45,6 +48,7 @@ export default defineConfig({
         enabled: true,
       },
       injectRegister: "auto",
+    
     }),
   ],
 });
